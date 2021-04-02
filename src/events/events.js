146ba86c8d32847bfd5f94627491892330078152
kiizerd <ajsewell13@gmail.com>
2021-04-02 18:15:01 -0500

    added eventAggregator module to loosen coupling

---
+++ src/events/events.js
@@ -0,0 +1,47 @@
+function Event (name) {
+  this._handlers = [];
+  this.name = name;
+}
+
+Event.prototype.addHandler = function(handler) {
+  this._handlers.push(handler);
+};
+
+Event.prototype.removeHandler = function(handler) {
+  for (let i = 0; i < this._handlers.length; i++) {
+    if (this._handlers[i] == handler) {
+      this._handlers.splice(i, 1);
+      break;
+    }
+  }
+};
+
+Event.prototype.fire = function(eventArgs) {
+  this._handlers.forEach(handler => {
+    handler(eventArgs);
+  });
+};
+
+const eventAggregator = (function() {
+  const events = [];
+
+  function getEvent(eventName) {
+    for (let i = 0; i < events.length; i++) {
+      if (events[i].name === eventName) return events[i]
+    }
+    console.log('event not found')
+    return new Event(eventName);
+  };
+
+  function publish(eventName, eventArgs) {
+    getEvent(eventName).fire(eventArgs);
+  };
+
+  function subscribe(eventName, handler) {
+    getEvent(eventName).addHandler(handler);
+  };
+
+  return { publish, subscribe };
+})();
+
+export { eventAggregator }
