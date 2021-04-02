e7b02b17003a95b31de0552ef163c1bbbecdb15a
kiizerd <ajsewell13@gmail.com>
2021-04-02 18:18:43 -0500

    implemented eventAggregator and fleshed out some of homepage

--- src/display/display.js
+++ src/display/display.js
@@ -1,26 +1,41 @@
 import { getHeader } from './displayHeader';
-import { getHomePage } from './displayHomePage';
+import { getHomePage } from './displayHome';
 
 const Display = (function() {
+  const content = document.getElementById('content');
 
   function init() {
-    const header = getHeader(),
-          homePage = getHomePage();
-
-    const content = document.getElementById('content');
-    content.setActivePage = pageName => {
-      let page = (pageName === 'home') ? homePage : getProjectPage(pageName);
-      content.innerHTML = '';
-      content.append(header, page);
-      window.scrollTo(0, 0);
-    }
-
-    content.setActivePage('home');
+    setActivePage('home');
+    content.setActivePage = setActivePage
   }
 
-  function getProjectPage(project) {}
+  function setActivePage(pageName) {
+    let page = getPage(pageName);
+    content.innerHTML = '';
+    content.append(getHeader(), page());
+    window.scrollTo(0, 0);
+    
+    console.log('going to -->> ' + pageName);
+  }
+
+  function getPage(name) {
+    const pages = {
+      'home': getHomePage,
+      'history': getHistoryPage
+    }
+
+    return !pages[name] ? getProjectPage.bind(name) : pages[name];
+  }
+
+  function getProjectPage() {
+    console.log('GET - project: ' + this);
+  }
 
   function getTaskCard(task) {}
+
+  function getHistoryPage() {
+    console.log('GET - History Page - 404 - Page not made lol')
+  }
 
   return { init }
 })();
---
+++ src/display/displayHome.js
@@ -0,0 +1,70 @@
+import { Generator } from '../generator';
+import { eventAggregator } from '../events/events';
+const Isotope = require('isotope-layout');
+
+const Display = (function() {
+
+  function getHomePage() {
+    const div = getProjectsContainer();    
+    const projectCards = getProjectCards()
+
+    div.append(projectCards);
+
+    return div;
+  }
+
+  function getProjectsContainer() {
+    const div = document.createElement('div');
+    div.classList.add('container-fluid');
+    div.id = 'projects-container';
+
+    let iso = new Isotope(div, {
+      itemSelector: '.projects-item',
+      layoutMode: 'fitRows'
+    });
+
+    return div
+  }
+
+  function getProjectCards(options) {
+    const projectList = [];
+    const cardList = [];
+    
+    eventAggregator.publish('requestProjects', options ? options : {
+      sort: 'default',
+      filter: 'default'
+    });
+    
+    eventAggregator.subscribe('projectsReceipt', projects => {
+      // reset local list to guarantee most up to date information
+      projectList.length = 0;
+
+      projects.forEach(project => projectList.push(project));
+    });
+
+    for (let i = 0; i < projectList.length; i++) {
+      let card = createProjectCard(projectList[i]);
+      cardList.push(card);
+    };
+
+    function createProjectCard(project) {
+      const card = Generator.createCard()
+      fillCard(card, project)
+      card.classList.add('projects-item', 'bg-dark', 'text-white');
+
+      return card
+    }
+
+    return cardList
+  }
+
+  function fillCard(card, project) {
+    console.log(card, project)
+  }
+  
+  return { getHomePage }
+})()
+
+const getHomePage = Display.getHomePage;
+
+export { getHomePage }
