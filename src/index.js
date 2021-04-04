79d5e6e37aa07e91307a1abf3d8e0137a2e14a5b
kiizerd <ajsewell13@gmail.com>
2021-04-03 23:20:58 -0500

    imported modules and created mock projects

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import { Display } from './display/display';
 import { EventAggregator } from './events/events';
-import { Database } from './database'
-import { Controller } from './controllers/controller'
-import { projectInterface } from './interfaces/projectInterface'
+import { Display } from './display/display';
+import { Database } from './database';
+import { Controller } from './controllers/controller';
+import { projectInterface } from './interfaces/projectInterface';
 
 projectInterface;
 Database;
 Controller;
-Display.init();
 
 EventAggregator.publish('createProject', {
-  title: 'project 1',
+  title: 'Project 1',
   description: 'a small summary of the project',
   priority: 0,
-  dueDate: '2022-06-09'
+  dueDate: new Date(2069, 3, 8)
 });
+
+EventAggregator.publish('createProject', {
+  title: 'Project 12',
+  description: 'a small summary of the project not too big not too small',
+  priority: 1,
+  dueDate: new Date(2022, 1, 7)
+});
+
+EventAggregator.publish('createProject', {
+  title: 'Project 412',
+  description: 'a small summary of the project not too big not too small',
+  priority: 2,
+  dueDate: new Date(2022, 6, 9)
+});
+
+Display.init();
