cd5b4becea4fd3159b730d57f5ecd1882409ffb2
Alfred <ajsewell13@gmail.com>
2021-04-10 15:03:03 -0500

    changed factory to class funcs and added rough crud operations

--- src/interfaces/projectInterface.js
+++ src/interfaces/projectInterface.js
@@ -3,75 +3,84 @@ import { format } from 'date-fns'
 
 const projectInterface = (function() {
   
-  function Project(formData) {
-    this.title = formData.title;
-    this.description = formData.description;
-    this.priority = formData.priority;
-    this.dates = {
-      due: formData.due ? format(formData.due, 'PP') : null,
-      started: format(formData.started ? formData.started : new Date(), 'PP')
+  class Project {
+    constructor(formData) {
+      this.title = formData.title;
+      this.description = formData.description;
+      this.priority = parseInt(formData.priority);
+      
+      const dueDate = formData.dates.due;
+      const startDate = formData.dates.started;
+
+      console.log('project due date --', dueDate);
+      console.log('project start date --', startDate);
+
+      this.dates = {
+        due: dueDate ? format(new Date(dueDate), 'PP') : null,
+        started: startDate ? format(new Date(startDate), 'PP') : format(new Date(), 'PP')
+      };
+
+      this.tasks = [];
+      this.completed = false;
+      this.active = true;
     }
-    this.tasks = [];
-    this.completed = false;
-    this.active = true;
-  };
 
-  Project.prototype.getTask = function(taskName) {
-    for (const task of this.tasks) {
-      if (task.title = taskName) {
-        return task
+    getTask(taskName) {
+      for (const task of this.tasks) {
+        if (task.title = taskName) {
+          return task;
+        };
       };
-    };
 
-    return new Error('Task not found');
-  };
+      return new Error('Task not found');
+    }
 
-  Project.prototype.addTask = function(taskObj) {
-    this.tasks.push(taskObj);
-  };
+    addTask(taskObj) {
+      this.tasks.push(taskObj);
+    }
 
-  Project.prototype.completeTask = function(taskName) {
-    const task = this.getTask(taskName);
+    completeTask(taskName) {
+      const task = this.getTask(taskName);
 
-    EventAggregator.publish('taskCompleted', {
-      task: task,
-      project: this
-    });
+      EventAggregator.publish('taskCompleted', {
+        task: task,
+        project: this
+      });
 
-    task.completeTask();
-  };
+      task.completeTask();
+    }
 
-  Project.prototype.removeTask = function(taskName) {
-    const task = this.getTask(taskName);
-    const index = this.tasks.indexOf(task);
+    removeTask(taskName) {
+      const task = this.getTask(taskName);
+      const index = this.tasks.indexOf(task);
 
-    
-    task.active = false;
-    this.task.splice(index, 1);
 
-    EventAggregator.publish('taskDeleted', task);
-  };
+      task.active = false;
+      this.task.splice(index, 1);
 
-  Project.prototype.setProperties = function(newProps) {
-    for (const prop in newProps) {
-      if (this[prop]) {
-        this[prop] = newProps[prop];
+      EventAggregator.publish('taskDeleted', task);
+    }
+
+    setProperties(newProps) {
+      for (const prop in newProps) {
+        if (this[prop]) {
+          this[prop] = newProps[prop];
+        };
       };
-    };
-  };
+    }
 
-  Project.prototype.completeProject = function() {
-    if (this.tasks.length === 0)
-    EventAggregator.publish('projectCompleted', this)
-  };
+    completeProject() {
+      if (this.tasks.length === 0)
+        EventAggregator.publish('projectCompleted', this);
+    }
 
-  Project.prototype.deleteProject = function() {
-    // delete all of the projects tasks 
-    // database will remove project from listing
-
-    this.tasks.length = 0;
-    this.active = false;
-    EventAggregator.publish('projectDeleted', this);
+    deleteProject() {
+      // delete all of the projects tasks 
+      // database will remove project from listing
+      this.tasks.length = 0;
+      this.active = false;
+      EventAggregator.publish('projectDeleted', this);
+    }
   };
 
   EventAggregator.subscribe('formToProject', formData => {
--- src/interfaces/taskInterface.js
+++ src/interfaces/taskInterface.js
@@ -1,37 +1,46 @@
-import { EventAggregator } from '../events/events'
+import { EventAggregator } from '../events'
+import { format } from 'date-fns'
 
 const taskInterface = (function() {
   
-  function Task(formData) {
-    this.title = formData.title,
-    this.description = formData.description,
-    this.priority = formData.priority,
-    this.dates = {
-      due: formData.due ? format(formData.due, 'PP') : null,
-      started: format(formData.started ? formData.started : new Date(), 'PP')
-    };
-    this.completed = false;
-    this.active = true;
-  };
+  class Task {
+    constructor(formData) {
+      this.title = formData.title;
+      this.description = formData.description;
+      this.priority = parseInt(formData.priority);
 
-  Task.prototype.setProperties = function(newProps) {
-    for (const prop in newProps) {
-      if (this[prop]) {
-        task[prop] = newProps[prop];
+      const dueDate = formData.dates.due;
+      const startDate = formData.dates.started;
+
+      console.log('task due date --', dueDate);
+      console.log('task start date --', startDate);
+      
+      this.dates = {
+        due: dueDate ? format(new Date(dueDate), 'PP') : null,
+        started: startDate ? format(new Date(startDate), 'PP') : format(new Date(), 'PP')
+      };
+
+      this.completed = false;
+      this.active = true;
+    }
+
+    setProperties(newProps) {
+      for (const prop in newProps) {
+        if (this[prop]) {
+          task[prop] = newProps[prop];
+        };
       };
     };
+
+    completeTask() {
+      this.completed = true;
+    }
   };
-
-  Task.prototype.completeTask = function() {
-    this.completed = true;
-  }
-
 
 
   EventAggregator.subscribe('formToTask', formData => {
-    let newTask = Task(formData);
-
-    EventAggregator.priority('taskFromForm', newTask);
+    let newTask = new Task(formData);
+    EventAggregator.publish('taskCreated', newTask);
   });
 
 })();
