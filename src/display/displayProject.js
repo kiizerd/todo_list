9e0419ab8ecaaf4bfd670fe594702fff975b5f99
Alfred <ajsewell13@gmail.com>
2021-04-10 15:02:08 -0500

    refactored modal init funcs to be used by project and task modal

--- src/display/displayProject.js
+++ src/display/displayProject.js
@@ -6,37 +6,49 @@ const Display = (function() {
   EventAggregator.subscribe('newProjectClicked', () => {
     let modal = document.getElementById('new-project-modal');
     if (!modal) {
-      modal = getNewProjectModalObj();
+      modal = getNewProjectModal();
       document.body.append(modal);
     }
 
-    $('.ui.modal')
-      .modal('show')
-    ;
-
-    $('#new-form-priority-dropdown')
-      .dropdown()
-    ;
-
-    $('.ui.checkbox')
-      .checkbox()
-    ;
-
-    $('.ui.calendar').calendar({
-      type: 'date'
-    });
-
-    addConfirmClickEvent();
-
-    const initToggles = initFormToggles();
-    for (const toggle of initToggles) {
-      toggle();
-    }
+    initModal('project');
   });
 
-  function getNewProjectModalObj() {
+  EventAggregator.subscribe('newTaskClicked', () => {
+    let modal = document.getElementById('new-task-modal');
+    if (!modal) {
+      modal = getNewTaskModal()
+      document.body.append(modal);
+    }
+    
+    initModal('task');
+  });
+
+  function getNewTaskModal() {
+    const modal = Generator.createModal();
+    modal.id = 'new-task-modal';
+    modal.classList.add('tiny');
+
+    modal.header.textContent = 'New Task';
+
+    const modalForm = getNewTaskForm();
+
+    modal.content.append(modalForm);
+
+    return modal
+  }
+
+  function getNewTaskForm() {
+    const form = Generator.createForm();
+    form.id = 'new-task-form';
+    form.onsubmit = "return false";
+
+    return form
+  }
+
+  function getNewProjectModal() {
     const modal = Generator.createModal();
     modal.id = 'new-project-modal';
+    modal.classList.add('small');
 
     modal.header.textContent = 'New Project';
 
@@ -55,24 +67,17 @@ const Display = (function() {
     return form
   };
 
-  function addConfirmClickEvent() {
-    const confirmBtn = document.querySelector('#new-project-modal .actions .positive');
-    confirmBtn.onclick = () => { 
-      const formData = getNewProjectFormData();
-      EventAggregator.publish('createProject', formData);
-    }
-  }
-
-  function getNewProjectFormData() {
-    const form = document.forms['new-project-form'];
+  function getNewFormData(formName) {
+    const formId = 'new-' + `${formName}` + '-form'
+    const form = document.forms[formId];
 
     return {
       title: form.elements[0].value,
       priority: form.elements[1].value,
       description: form.elements[2].value,
       dates: {
-        started: form.elements[4],
-        due: form.elements[5]
+        started: form.elements[3].value,
+        due: form.elements[5].value
       }
     };
 
@@ -82,39 +87,85 @@ const Display = (function() {
     
   };
 
-  function initFormToggles() {
-    function initDueDateToggle() {
-      const fieldId = 'new-form-dueDateField';
-      const toggleId = 'new-form-dueDateToggle';
-      const dueDateToggle = document.getElementById(toggleId);
-      dueDateToggle.classList.remove('hidden');
-      dueDateToggle.onclick = () => {
-        const dueDateField = document.getElementById(fieldId);
-        if (dueDateToggle.checked) {
-          dueDateField.classList.remove('disabled');
-        } else {
-          dueDateField.classList.add('disabled');
-        };
-      };
+  function initModal(modal) {
+    $('#new-' + `${modal}` + '-modal')
+      .modal('show')
+    ;
+
+    $('#new-' + `${modal}` + '-modal #new-form-priority-dropdown')
+      .dropdown()
+    ;
+
+    $('.ui.checkbox')
+      .checkbox()
+    ;
+
+    $('.ui.calendar').calendar({
+      type: 'date'
+    });
+    
+    addConfirmClickEvents();
+    
+    const initToggles = initFormToggles(modal);
+    for (const toggle of initToggles) {
+      toggle();
     };
 
-    function initDateStartedToggle() {
-      const fieldId = 'new-form-dateStartedField';
-      const toggleId = 'new-form-dateStartedToggle';
-      const dateStartedToggle = document.getElementById(toggleId);
-      dateStartedToggle.classList.remove('hidden');
-      dateStartedToggle.onclick = () => {
-        const dateStartedField = document.getElementById(fieldId);
-        if (dateStartedToggle.checked) {
-          dateStartedField.classList.add('disabled');
-        } else {
-          dateStartedField.classList.remove('disabled');
+
+    function addConfirmClickEvents() {      
+      if (modal === 'project') {
+        const projectConfirmBtn = document.querySelector('#new-project-modal .actions .positive');
+        projectConfirmBtn.onclick = () => { 
+          const formData = getNewFormData('project');
+          console.log(formData)
+          EventAggregator.publish('createProject', formData);
+        };
+
+      } else if (modal === 'task') {
+        const taskConfirmBtn = document.querySelector('#new-task-modal .actions .positive');
+        taskConfirmBtn.onclick = () => {
+          const formData = getNewFormData('task');
+          EventAggregator.publish('createTask', formData);
         };
       };
+
     };
 
-    return [initDueDateToggle, initDateStartedToggle]
-  };
+    function initFormToggles(modal) {
+      const modalId = '#new-' + `${modal}` + '-modal'
+      function initDueDateToggle() {
+        const fieldId = '#new-form-dueDateField';
+        const toggleId = '#new-form-dueDateToggle';
+        const dueDateToggle = document.querySelector(modalId + ' ' + toggleId);
+        dueDateToggle.classList.remove('hidden');
+        dueDateToggle.onclick = () => {
+          const dueDateField = document.querySelector(modalId + ' ' + fieldId);
+          if (dueDateToggle.checked) {
+            dueDateField.classList.remove('disabled');
+          } else {
+            dueDateField.classList.add('disabled');
+          };
+        };
+      };
+
+      function initDateStartedToggle() {
+        const fieldId = '#new-form-dateStartedField';
+        const toggleId = '#new-form-dateStartedToggle';
+        const dateStartedToggle = document.querySelector(modalId + ' ' + toggleId);
+        dateStartedToggle.classList.remove('hidden');
+        dateStartedToggle.onclick = () => {
+          const dateStartedField = document.querySelector(modalId + ' ' + fieldId);
+          if (dateStartedToggle.checked) {
+            dateStartedField.classList.add('disabled');
+          } else {
+            dateStartedField.classList.remove('disabled');
+          };
+        };
+      };
+
+      return [initDueDateToggle, initDateStartedToggle]
+    };
+  }
 
   return { getProjectPage }
 })()
