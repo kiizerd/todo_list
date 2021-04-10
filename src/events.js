05ee8b956f2c53df4d03b410700ce0a82c438379
Alfred <ajsewell13@gmail.com>
2021-04-10 14:58:46 -0500

    add Token and changed factory funcs to class funcs

--- src/events.js
+++ src/events.js
@@ -1,34 +1,60 @@
-function Event (name) {
-  this._handlers = [];
-  this.name = name;
-}
+class Event {
+  constructor(name) {
+    this._handlers = [];
+    this.name = name;
+  };
 
-Event.prototype.addHandler = function(handler) {
-  this._handlers.push(handler);
+  addHandler(handler) {
+    let check = true
+    for (let i = 0; i < this._handlers.length; i++) {
+      if (this._handlers[i] == handler) { check = false }
+    };
+    if (check) this._handlers.push(handler);
+    else return false
+  };
+
+  removeHandler(handler) {
+    for (let i = 0; i < this._handlers.length; i++) {
+      if (this._handlers[i] === handler) {
+        this._handlers.splice(i, 1);
+        break;
+      };
+    };
+  };
+
+  fire(eventArgs) {
+    this._handlers.forEach(handler => {
+      handler(eventArgs);
+    });
+  };
+
 };
 
-Event.prototype.removeHandler = function(handler) {
-  for (let i = 0; i < this._handlers.length; i++) {
-    if (this._handlers[i] == handler) {
-      this._handlers.splice(i, 1);
-      break;
-    }
-  }
+class Token {
+  constructor(event, master) {
+    let tokenCounter = 0;
+    let idString = this.generateId(`${event}_${master}`);
+    this._id = `${idString}` + `${tokenCounter++}`;
+  };
+
+  generateId(string) {
+    const result = []
+    for (const char in string.split('')) {
+      result.push(string.charCodeAt(char));
+    };
+    return result.join('')
+  };
 };
 
-Event.prototype.fire = function(eventArgs) {
-  this._handlers.forEach(handler => {
-    handler(eventArgs);
-  });
-};
 
 const EventAggregator = (function() {
   const events = [];
+  let handlerTagCounter = 0;
 
   function getEvent(eventName) {
     for (let i = 0; i < events.length; i++) {
-      if (events[i].name === eventName) return events[i]
-    }
+      if (events[i].name === eventName) return events[i];
+    };
   };
 
   function publish(eventName, eventArgs) {
@@ -49,11 +75,11 @@ const EventAggregator = (function() {
       event = new Event(eventName);
       events.push(event);
     }
-
+    
     event.addHandler(handler);
   };
 
   return { publish, subscribe };
 })();
 
-export { EventAggregator }
+export { EventAggregator, Token }
