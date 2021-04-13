4dbea82fe30b9603a8f1df8e69529911a0be5dbc
kiizerd <ajsewell13@gmail.com>
2021-04-12 22:25:19 -0500

    moved sample projects into demoProjects function

--- src/index.js
+++ src/index.js
@@ -5,39 +5,48 @@ import { Controller } from './controllers/controller';
 import { projectInterface } from './interfaces/projectInterface';
 import { taskInterface } from './interfaces/taskInterface';
 
-projectInterface;
-taskInterface;
-Database;
-Controller;
+const init = [
+  projectInterface,
+  taskInterface,
+  Controller
+];
 
+Database.setupStorage();
 Display.init();
 
-EventAggregator.publish('createProject', {
-  title: 'General',
-  description: 'A non-specific list of generic tasks.',
-  priority: 2,
-  dates: {
-    started: '',
-    due: ''
-  }
-});
+function demoProjects() {
 
-EventAggregator.publish('createProject', {
-  title: 'Work',
-  description: 'Tasks that need to be completed for your job.',
-  priority: 0,
-  dates: {
-    started: '',
-    due: new Date(2021, 4, 20)
-  } 
-});
+  EventAggregator.publish('createProject', {
+    title: 'General',
+    description: 'A non-specific list of generic tasks.',
+    priority: 2,
+    dates: {
+      started: '',
+      due: ''
+    }
+  });
 
-EventAggregator.publish('createProject', {
-  title: 'House',
-  description: 'A list of household chores and duties.',
-  priority: 1,
-  dates: {
-    started: '',
-    due: ''
-  } 
-});
+  EventAggregator.publish('createProject', {
+    title: 'Work',
+    description: 'Tasks that need to be completed for your job.',
+    priority: 0,
+    dates: {
+      started: '',
+      due: new Date(2021, 4, 20)
+    }
+  });
+
+  EventAggregator.publish('createProject', {
+    title: 'House',
+    description: 'A list of household chores and duties.',
+    priority: 1,
+    dates: {
+      started: '',
+      due: ''
+    }
+
+  });
+  
+};
+
+// demoProjects();
