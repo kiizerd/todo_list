f314b49d824bc5e8a8417db79bc70d0af969259f
kiizerd <ajsewell13@gmail.com>
2021-04-16 13:34:04 -0500

    made projects editable and completable

--- src/database.js
+++ src/database.js
@@ -27,35 +27,55 @@ const Database = (function() {
     return projects
   };
 
+  function getStoredCompleted() {
+    const localStorage = JSON.parse(window.localStorage.getItem('StorageObject'));
+
+    const completed = localStorage.projects
+
+    return completed.length ? completed : null;
+  }
+
   function loadStorage() {
     const storedProjects = getStoredProjects();
-    if (Object.entries(storedProjects).length) {
-      for (const project in storedProjects) {
-        const current = storedProjects[project];     
-        
-        if (current.tasks.length) {
-          const newTaskList = current.tasks.map(task => {
-            EventAggregator.subscribe('taskFromStorage', newTask => {
-              task = newTask;
+    const storedCompleted = getStoredCompleted();
+
+    loadProjects(storedProjects);
+
+    if (storedCompleted) loadProjects(storedCompleted);
+    
+    function loadProjects(list) {
+      if (Object.entries(list).length) {
+        for (const project in list) {
+          const current = list[project];     
+          
+          if (current.tasks.length) {
+            const newTaskList = current.tasks.map(task => {
+              EventAggregator.subscribe('taskFromStorage', newTask => {
+                task = newTask;
+              });
+              
+              EventAggregator.publish('storedTaskToTask', task);
+              
+              return task
             });
-            
-            EventAggregator.publish('storedTaskToTask', task);
-            
-            return task
+  
+            current.tasks = newTaskList
+          };
+  
+          EventAggregator.subscribe('projectFromStorage', projectObj => {
+            if (list === storedProjects) {
+              projects[projectObj.title] = projectObj;
+            } else {
+              completedProjects[projectOjb.title] = projectObj;
+            };
           });
-
-          current.tasks = newTaskList
+  
+          EventAggregator.publish('storedProjectToProject', current);
+  
         };
-
-        EventAggregator.subscribe('projectFromStorage', projectObj => {
-          projects[projectObj.title] = projectObj;
-        });
-
-        EventAggregator.publish('storedProjectToProject', current);
-
       };
     };
-    
+
   };
   
   function setupStorage() {
@@ -64,39 +84,20 @@ const Database = (function() {
     }));
   };
 
-  function checkStorageFor(projectName) {
-    const stored = getStoredProjects();
-    const storedAry = Object.entries(stored);
-    
-    let check = true;
-    for (const project of storedAry) {
-      if (project.title === projectName) check = false
-    }
-
-    return check
-  };
-
   function updateStorage() {
     setTimeout(() => {
       window.localStorage.setItem('StorageObject', JSON.stringify({
-        'projects': projects
+        'projects': projects,
+        'completedProjects': completedProjects
       }));
-    }, 500);
-  };
+    }, 200);
 
-  function storeProject(newProject) {
-    const stored = getStoredProjects();
+    setTimeout(() => {
+      EventAggregator.publish('updateDisplay', []);
+    }, 400);
     
-    if (checkStorageFor(newProject.title)) {
-      stored[newProject.title] = newProject;
-      console.log('storing new project');
-      
-      window.localStorage.setItem('StorageObject', JSON.stringify({
-        projects: stored
-      }));
-    };
-
   };
+
   
   EventAggregator.subscribe('projectCreated', projectObj => {    
     projects[projectObj.title] = projectObj;
@@ -107,61 +108,10 @@ const Database = (function() {
   EventAggregator.subscribe('taskCreated', taskObj => {
     updateStorage();
   });
-
-
-  // function completeStoredProject() {
-
-  // };
-
-  // function completeProject(project) {
-  //   completedProjects[project.title] = project;
-
-  //   project.active = false;
-  //   project.completed = true;
-
-  //   deleteProject(project.title);
-  // };
-
-  // EventAggregator.subscribe('projectCompleted', completedProject => {
-  //   completeProject(project);
-  // });
-
-  // function updateStoredProject(projectName, updatedProject) {
-  //   const stored = getStoredProjects();
-
-  //   if (!checkStorageFor(projectName)) {
-  //     if (projectName === updatedProject.title) {
-  //       stored[projectName] = updatedProject;
-  //     } else {
-  //       delete stored[projectName];
-  //       stored[updatedProject.title] = updatedProject;
-  //     };
-  //   };
-
-  //   window.localStorage.setItem('StorageObject', JSON.stringify({
-  //     projects: stored
-  //   }));
-
-  // };
-
-
-  // function updateProject(projectName, updatedProject) {
-  //   if (projectName === updatedProject.title) {
-  //     projects[projectName] = updatedProject;
-  //   } else {
-  //     delete projects[projectName];
-  //     projects[updatedProject.title] = updatedProject;
-  //   };
-  // };
-
-  // EventAggregator.subscribe('projectUpdated', (projectName, updatedProject) => {
-  //   updateStoredProject(projectName, updatedProject);
-  //   updateProject(projectName, updatedProject);
-  // });
-
   
-  EventAggregator.subscribe('projectDeleted', projectName => {
-    delete projects[projectName];
+
+  EventAggregator.subscribe('projectDeleted', project => {
+    delete projects[project.title];
     updateStorage();
   });
 
@@ -169,6 +119,39 @@ const Database = (function() {
   EventAggregator.subscribe('taskDeleted', taskName => {
     updateStorage();
   });
+
+
+  function updateProject(projectName, updatedProject) {
+    if (projectName === updatedProject.title) {
+      projects[projectName] = updatedProject;
+    } else {
+      delete projects[projectName];
+      projects[updatedProject.title] = updatedProject;
+    };
+
+    updateStorage();
+  };
+
+  
+  EventAggregator.subscribe('projectUpdated', ([projectName, updatedProject]) => {
+    updateProject(projectName, updatedProject);
+  });
+
+
+  function completeProject(project) {
+    project.active = false;
+    project.completed = true;
+
+    completedProjects[project.title] = project;
+    
+    project.deleteProject();
+  };
+
+
+  EventAggregator.subscribe('projectCompleted', project => {
+    completeProject(project);
+  });
+
 
 
   // options = {
@@ -205,6 +188,7 @@ const Database = (function() {
 
   EventAggregator.subscribe('requestProjects', options => {
     const reqProjects = Object.values(projects);
+    console.log('reqProjects', reqProjects);
     const filteredProjects = filterProjects(options.filter, reqProjects);
     const sortedProjects = sortProjects(options.sort, filteredProjects);
 
