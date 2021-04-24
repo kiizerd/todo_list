4c71efa2d3dbf870b5e9c7b454823abc1c76a206
kiizerd <ajsewell13@gmail.com>
2021-04-23 22:11:25 -0500

    passed ESLint

--- src/database.js
+++ src/database.js
@@ -1,154 +1,139 @@
-import { EventAggregator } from './events'
+import { EventAggregator } from './events/events';
 
-const Database = (function() {
-
+const Database = ((function iife() {
   const projects = {};
 
   const completed = {
     projects: {},
-    tasks: {}
+    tasks: {},
   };
+
+  function checkForStorage() {
+    return (window.localStorage.getItem('StorageObject') != null);
+  }
+
+  function getStoredProjects() {
+    const localStorage = JSON.parse(window.localStorage.getItem('StorageObject'));
+
+    const storedProjects = localStorage.projects;
+
+    return storedProjects || null;
+  }
+
+  function getCompletedProjects() {
+    const localStorage = JSON.parse(window.localStorage.getItem('StorageObject'));
+
+    if (!localStorage.completed) return null;
+
+    const completedProjects = localStorage.completed.projects;
+
+    return completedProjects || null;
+  }
+
+  function getCompletedTasks() {
+    const localStorage = JSON.parse(window.localStorage.getItem('StorageObject'));
+
+    if (!localStorage.completed) return null;
+
+    const completedTasks = localStorage.completed.tasks;
+
+    return completedTasks || null;
+  }
+
+  function loadStorage() {
+    const storage = [
+      getStoredProjects(),
+      getCompletedProjects(),
+      getCompletedTasks(),
+    ];
+
+    Object.entries(storage).forEach((stored) => {
+      if (stored) {
+        stored.forEach((object) => {
+          if (stored === storage[0]) {
+            if (stored[object].tasks && stored[object].tasks.length) {
+              EventAggregator.publish('storedTasksToTasks', [
+                stored[object].tasks,
+                stored[object].title,
+              ]);
+            }
+            EventAggregator.publish('storedProjectToProject', stored[object]);
+          } else if (stored === storage[1]) {
+            completed.projects[stored[object].title] = stored[object];
+          } else if (stored === storage[2]) {
+            completed.tasks[stored[object].title] = stored[object];
+          }
+        });
+      }
+    });
+  }
+
+  function setupStorage() {
+    window.localStorage.setItem('StorageObject', JSON.stringify({
+      projects: {},
+    }));
+  }
+
+  function updateStorage() {
+    setTimeout(() => {
+      window.localStorage.setItem('StorageObject', JSON.stringify({
+        projects,
+        completed,
+      }));
+
+      EventAggregator.publish('storageUpdated', []);
+    }, 50);
+
+    setTimeout(() => {
+      EventAggregator.publish('updateDisplay', []);
+    }, 100);
+  }
 
   function initStorage() {
     if (checkForStorage()) {
       loadStorage();
     } else {
       setupStorage();
-    };
+    }
 
     setTimeout(() => {
       updateStorage();
     }, 75);
-    
-  };
+  }
 
-  function checkForStorage() {
-    return (window.localStorage.getItem('StorageObject') != null);
-  };
-
-  function getStoredProjects() {
-    const localStorage = JSON.parse(window.localStorage.getItem('StorageObject'));
-    
-    const projects = localStorage.projects
-
-    return projects ? projects : null;
-  };
-
-  function getCompletedProjects() {
-    const localStorage = JSON.parse(window.localStorage.getItem('StorageObject'));
-
-    if (!localStorage.completed) return null
-
-    const completed = localStorage.completed.projects;
-
-    return completed ? completed : null;
-  };
-
-  function getCompletedTasks() {
-    const localStorage = JSON.parse(window.localStorage.getItem('StorageObject'));
-
-    if (!localStorage.completed) return null
-
-    const completed = localStorage.completed.tasks;
-    
-
-    return completed ? completed : null;
-  };
-  
-  
-  EventAggregator.subscribe('projectFromStorage', projectObj => {
+  EventAggregator.subscribe('projectFromStorage', (projectObj) => {
     if (projects[projectObj.title]) {
-      projectObj.tasks = projects[projectObj.title].tasks;
-    };
+      const project = projectObj;
+      project.tasks = projects[projectObj.title].tasks;
+    }
 
     projects[projectObj.title] = projectObj;
   });
 
-  
-  EventAggregator.subscribe('tasksFromStorage', args => {
+  EventAggregator.subscribe('tasksFromStorage', (args) => {
     const newTaskList = args[0];
     const project = args[1];
 
     projects[project] = { tasks: newTaskList };
   });
 
-
-  function loadStorage() {
-    const storage = [
-      getStoredProjects(),
-      getCompletedProjects(),
-      getCompletedTasks()
-    ];
-
-    for (const stored of storage) {
-      if (stored) {
-        for (const object in stored) {
-          if (stored === storage[0]) {
-            
-            if (stored[object].tasks && stored[object].tasks.length) {
-              EventAggregator.publish('storedTasksToTasks', [
-                stored[object].tasks, 
-                stored[object].title
-              ]);
-            };
-            
-            EventAggregator.publish('storedProjectToProject', stored[object]);
-
-          } else if (stored === storage[1]) {
-            completed.projects[stored[object].title] = stored[object];
-          } else if (stored === storage[2]) {
-            completed.tasks[stored[object].title] = stored[object];
-          };
-
-        };
-      };
-    };
-    
-  };
-  
-  function setupStorage() {
-    window.localStorage.setItem('StorageObject', JSON.stringify({
-      'projects': {}
-    }));
-  };
-
-  function updateStorage() {
-    setTimeout(() => {
-      window.localStorage.setItem('StorageObject', JSON.stringify({
-        'projects': projects,
-        'completed': completed
-      }));
-
-      EventAggregator.publish('storageUpdated', []);
-    }, 50);    
-
-    setTimeout(() => {
-      EventAggregator.publish('updateDisplay', []);
-    }, 100);
-  };
-
-  
-  EventAggregator.subscribe('projectCreated', projectObj => {    
+  EventAggregator.subscribe('projectCreated', (projectObj) => {
     projects[projectObj.title] = projectObj;
     updateStorage();
   });
 
-  
-  EventAggregator.subscribe('taskCreated', taskObj => {
+  EventAggregator.subscribe('taskCreated', () => {
     updateStorage();
   });
-  
 
-  EventAggregator.subscribe('projectDeleted', project => {
+  EventAggregator.subscribe('projectDeleted', (project) => {
     delete projects[project.title];
     updateStorage();
   });
 
-
-  EventAggregator.subscribe('taskDeleted', taskName => {
+  EventAggregator.subscribe('taskDeleted', () => {
     updateStorage();
   });
-
 
   function updateProject(projectName, updatedProject) {
     if (projectName === updatedProject.title) {
@@ -156,50 +141,41 @@ const Database = (function() {
     } else {
       delete projects[projectName];
       projects[updatedProject.title] = updatedProject;
-    };
+    }
 
     updateStorage();
-  };
+  }
 
-  
   EventAggregator.subscribe('projectUpdated', ([projectName, updatedProject]) => {
     updateProject(projectName, updatedProject);
   });
 
-
-  EventAggregator.subscribe('taskUpdated', args => {
-    const task = args[0];
-    const project = args[1];
-
+  EventAggregator.subscribe('taskUpdated', () => {
     updateStorage();
   });
 
-
   function completeProject(project) {
     completed.projects[project.title] = project;
-    
+
     project.deleteProject();
-  };
+  }
 
-
-  EventAggregator.subscribe('projectCompleted', project => {
+  EventAggregator.subscribe('projectCompleted', (project) => {
     completeProject(project);
   });
 
-  
-  EventAggregator.subscribe('taskCompleted', args => {
+  EventAggregator.subscribe('taskCompleted', (args) => {
     const task = args[0];
     const project = args[1];
 
-    task.project = project
+    task.project = project;
 
-    completed.tasks[task.title] = task
+    completed.tasks[task.title] = task;
 
     setTimeout(() => {
       updateStorage();
     }, 75);
   });
-
 
   EventAggregator.subscribe('clearHistory', () => {
     completed.projects = {};
@@ -207,12 +183,10 @@ const Database = (function() {
     updateStorage();
   });
 
-
-
   // options = {
   //   filter: {
   //     byName: '', // projectName - will return projects with title matching string
-  //     byPriority: '', // 0 1 or 2 - will return projects with matching priority 
+  //     byPriority: '', // 0 1 or 2 - will return projects with matching priority
   //     byDateDue: '',
   //     byDateStarted: ''
   //   },
@@ -220,92 +194,85 @@ const Database = (function() {
   //     byName: '', // 0 or 1
   //     byPriority: '', // for asc
   //     byDateDue: '', // or desc
-  //     byDateStarted: ''    
+  //     byDateStarted: ''
   //   }
   // }
 
   function filter(options, ary) {
     let result;
     if (options) {
-      console.log('filter options', options);
       if (options.byName) {
-        result = ary.filter(project => project.title === options.byName);
+        result = ary.filter((project) => project.title === options.byName);
       }
-    }
-    else return ary
-    return result ? result : [];
-  };
+    } else return ary;
+    return result || [];
+  }
 
   function sort(options, ary) {
-    let result
+    let result;
     if (options) {
-      console.log('sort options', options);
       if (options.byName) {
-        result = ary.sort((a, b) => {          
+        result = ary.sort((a, b) => {
           const aTitle = [...a.title];
           const bTitle = [...b.title];
 
           let sortResult;
           if (options.byName === 'desc') {
-            sortResult = aTitle == bTitle ? 0 : aTitle < bTitle ? -1 : 1;
+            // FIX ME - Need clean alternative to nested ternary
+            // eslint-disable-next-line no-nested-ternary
+            sortResult = aTitle === bTitle ? 0 : aTitle < bTitle ? -1 : 1;
           }
           if (options.byName === 'asc') {
-            sortResult = aTitle == bTitle ? 0 : aTitle > bTitle ? -1 : 1;
+            // FIX ME - Need clean alternative to nested ternary
+            // eslint-disable-next-line no-nested-ternary
+            sortResult = aTitle === bTitle ? 0 : aTitle > bTitle ? -1 : 1;
           }
-          
-          return sortResult
+
+          return sortResult;
         });
-      };
-    } else return ary
-    return result
-  };
+      }
+    } else return ary;
+    return result;
+  }
 
   function getList(list, options) {
-    let result
-    switch (list) {
-      case 'projects':
-        result = Object.values(projects);
-        break;
-      case 'completedProjects':
-        result = Object.values(completed.projects);
-        break;
-      case 'completedTasks':
-        result = Object.values(completed.tasks);
-        break;
-    };
+    let result;
+    if (list === 'projects') {
+      result = Object.values(projects);
+    } else if (list === 'completedProjects') {
+      result = Object.values(completed.projects);
+    } else if (list === 'completedTasks') {
+      result = Object.values(completed.tasks);
+    }
 
     return sort(options.sort, filter(options.filter, result));
-  };
+  }
 
-
-  EventAggregator.subscribe('requestProjects', options => {
+  EventAggregator.subscribe('requestProjects', (options) => {
     const result = getList('projects', options);
 
-    if (options._token) result._token = options._token;
+    if (options.token) result.token = options.token;
 
     EventAggregator.publish('projectsReceipt', result);
   });
 
-
-  EventAggregator.subscribe('requestCompletedTasks', options => {
+  EventAggregator.subscribe('requestCompletedTasks', (options) => {
     const result = getList('completedTasks', options);
 
-    if (options._token) result._token = options._token;
+    if (options.token) result.token = options.token;
 
     EventAggregator.publish('completedTasksReceipt', result);
   });
 
-
-  EventAggregator.subscribe('requestCompletedProjects', options => {    
+  EventAggregator.subscribe('requestCompletedProjects', (options) => {
     const result = getList('completedProjects', options);
 
-    if (options._token) result._token = options._token;
+    if (options.token) result.token = options.token;
 
     EventAggregator.publish('completedProjectsReceipt', result);
   });
-  
-  return { initStorage }
 
-})();
+  return { initStorage };
+})());
 
-export { Database }
+export default Database;
